f92695a2331c371c348a2f8a40479cab1dcfc01c
Lury <lucasyuri56@hotmail.com>
2020-06-03 10:58:25 -0300

    portuguese fix

--- bot/bot-geral.js
+++ bot/bot-geral.js
@@ -10,7 +10,7 @@ module.exports = function(bot)
             }
 
         if (resp.toString().toLowerCase().includes(('pq' || 'por que') && ('vc' || 'você') && ('ta' || 'tá' || 'veio') && ('telegram'))) {
-            bot.sendMessage(chatId, `Pq estou morrendo de saudade de todo mundo na sede e também de um aglomeração rsrsrrs`);
+            bot.sendMessage(chatId, `Pq estou morrendo de saudade de todo mundo na sede e também de uma aglomeração rsrsrrs`);
            }
     
       }); 
