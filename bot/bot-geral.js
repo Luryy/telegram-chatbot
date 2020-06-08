feee205fa3cc96357c52e76140704c7e7b11eb6d
Lury <lucasyuri56@hotmail.com>
2020-06-08 10:24:26 -0300

    new mwnsages

--- bot/bot-geral.js
+++ bot/bot-geral.js
@@ -21,6 +21,15 @@ module.exports = function(bot)
          bot.sendMessage(chatId, `Infelizmente ainda não sei usar essa tecnologia, quem sabe em breve rsrsrs`);
         }
 
+        if (resp.toString().toLowerCase().includes('dia parado né')) {
+            bot.sendMessage(chatId, `Calma, eu estou descansando e amanhã volto com tudo, mas sempre com minha orelhinhas de lobo atentas aos leads, executando meus projetinhos e mandando o portfólio da EJEC pra todo mundo!`);
+        }
+
+        if (resp.toString().toLowerCase().includes('como faço para fazer uma boa precificação')) {
+            bot.sendMessage(chatId, `Primeiramente, converse com Adm-fin para tirar todas as dúvidas em relação a planilha, além de já ter feito um bom estudo de viabilidade com a galera de projetos e de ter colhido o máximo de informações com Negócios caso o lead tenha vindo através da redes sociais ou site.\n  
+            Outra dica top, é conversar com quem já executou bastante projeto igual ao que você está precificando. Boa precificação e negociação, esse lead já deixou de ser lead desde o momento em que conversou com você. Agora ele já é cliente!`);
+        }
+
     
       }); 
 }
